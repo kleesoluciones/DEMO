141036e48f2fa5b62022c96a8eed8d1ca843a68d
yaniravaldezv <yaniravaldezv@gmail.com>
2016-11-14 22:42:56 -0600

    Demo

--- Servidor/KSI_SocketServidor.cs
+++ Servidor/KSI_SocketServidor.cs
@@ -31,10 +31,7 @@ namespace Servidor
         #endregion
 
         #region Eventos
-        //public event ClienteConectado(EndPoint o)
-        //Public Event ClienteConectado(ByVal IDTerminal As Net.IPEndPoint)
-        //Public Event DatosRecibidos(ByVal IDTerminal As Net.IPEndPoint, ByRef datos As String)
-        //Public Event ClienteDesconectado(ByVal IpAddressClient As String, ByVal portCliente As Integer)
+        
         #endregion
         #region Propiedades
         public string PortListener { get { return sPortListener; } set { sPortListener = value; } }
@@ -58,7 +55,7 @@ namespace Servidor
                 DatosCliente oDataCurrentClient = new DatosCliente();
                 oDataCurrentClient.oTCPCliente = oTCPListener.AcceptTcpClient();
                 oIDClienteActual = oDataCurrentClient.oTCPCliente.Client.RemoteEndPoint;
-                //oDataCurrentClient.oThread = new Thread()
+                
                 oDataCurrentClient.sDatosRecibidos = "";
                 lock (this)
                 {
@@ -69,42 +66,7 @@ namespace Servidor
             }
         }
         
-    //        While True
-    //            Dim InfoClienteActual As New InfoDeUnCliente
-    //            With InfoClienteActual
-    //                'Cuando se recibe la conexion, guardo la informacion del cliente
-    //                'Guardo el Socket que utilizo para mantener la conexion con el cliente
-    //                .tcpClient = tcpLsn.AcceptTcpClient 'Se queda esperando la conexion de un cliente 
-
-    //                'Guardo el el RemoteEndPoint, que utilizo para identificar al cliente
-    //                IDClienteActual = .tcpClient.Client.RemoteEndPoint
-
-    //                'Creo un Thread para que se encargue de escuchar los mensaje del cliente 
-    //                .Thread = New Thread(AddressOf LeerSocket)
-
-    //                'Agrego la informacion del cliente al HashArray Clientes, donde esta la
-    //                .UltimosDatosRecibidos = ""
-    //                'informacion de todos estos
-    //                SyncLock Me
-    //                    Clientes.Add(IDClienteActual, InfoClienteActual)
-    //                End SyncLock
-
-    //                .Thread.Name = "Cliente" & .tcpClient.Client.RemoteEndPoint.ToString
-
-    //                ''Genero el evento Nueva conexion 
-    //                RaiseEvent ClienteConectado(IDClienteActual)
-
-    //                'Inicio el thread encargado de escuchar los mensajes del cliente 
-    //                .Thread.Start()
-    //            End With
-    //        End While
-    //    Catch ex As ThreadAbortException
-    //    Catch ex As SocketException
-    //    Finally
-    //        tcpLsn.Stop()
-    //        Console.WriteLine(Thread.CurrentThread.Name & " [END]")
-    //    End Try
-    //End Sub
+   
         #endregion
         #endregion
 
